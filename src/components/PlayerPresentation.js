5fff8166cf8a19138f1c8449f7b918fdbdaf9296
PhuPhuoc <blessforwork@gmail.com>
2023-06-07 18:55:36 +0700

    use State Hook in exer10

--- src/components/PlayerPresentation.js
+++ src/components/PlayerPresentation.js
@@ -1,17 +1,28 @@
-import React from 'react';
-export default function PlayerPresentation({players}) {
+import React, { useState } from 'react';
+export default function PlayerPresentation({ players }) {
+    const [player, setPlayer] = useState([]);
     return (
         <div className='container'>
             {players.map((player) => (
                 <div className='column'>
                     <div className='card'>
-                        <img src={player.img} alt='...'/>
+                        <img src={player.img} alt='...' />
                         <h3>{player.name}</h3>
                         <p className='title'>{player.club}</p>
-                        <p><button>Detail</button></p>
+                        <p><button onClick={() => { setPlayer(player) }}>
+                            <a href='#popup__info' id='openPopUp'>Detail</a>
+                        </button></p>
                     </div>
                 </div>
             ))}
+            <div id='popup__info' className='overlay'>
+                <div className='popup'>
+                    <img src={player.img} alt='...'></img>
+                    <h2>{player.name}</h2>
+                    <a className='close' href='#'>&times;</a>
+                    <div className='content'>{player.info}</div>
+                </div>
+            </div>
         </div>
     )
 }
